db52e4b95a81e84a893d290bf7b79592daec394f
Ahmed Mahboub <ahmed.mahboub99@gmail.com>
2021-07-17 18:23:56 +0200

    feat(task1): :sparkles: edit test

--- task1/test.py
+++ task1/test.py
@@ -1,77 +1,84 @@
-from PySide2 import QtCore
 from plotter import Window
+from PySide2 import QtCore
 import pytest
 
 
 @pytest.fixture
 def app(qtbot):
     tester = Window()
-    tester.show_err = False
+    tester.show_error_alert = False
     qtbot.addWidget(tester)
     return tester
 
 
-def empty_test(app, qtbot):
+def test_empty(app, qtbot):
     qtbot.mouseClick(app.plot_button, QtCore.Qt.LeftButton)
-    assert app.err == "All fileds are required"
+    assert app.error_messag == "error: All fileds are required"
 
-    app.f_x.setText("5*X^2+3*X")
-    app.min_x.setText("-10")
+    app.function_x.setText("5*X^2+3*X")
+    app.min_x.setText("-20")
     app.max_x.setText("")
     qtbot.mouseClick(app.plot_button, QtCore.Qt.LeftButton)
-    assert app.err == "All fileds are required"
+    assert app.error_messag == "error: All fileds are required"
 
-    app.f_x.setText("5*X^2+3*X")
+    app.function_x.setText("5*X^2+3*X")
     app.min_x.setText("")
-    app.max_x.setText("10")
+    app.max_x.setText("20")
     qtbot.mouseClick(app.plot_button, QtCore.Qt.LeftButton)
-    assert app.err == "All fileds are required"
+    assert app.error_messag == "error: All fileds are required"
 
-    app.f_x.setText("")
-    app.min_x.setText("-10")
-    app.max_x.setText("10")
+    app.function_x.setText("")
+    app.min_x.setText("-20")
+    app.max_x.setText("20")
     qtbot.mouseClick(app.plot_button, QtCore.Qt.LeftButton)
-    assert app.err == "error: All fileds are required"
+    assert app.error_messag == "error: All fileds are required"
 
 
-def invalid_function(app, qtbot):
-    app.f_x.setText("5*y^2+2*x")
-    app.min_x.setText("-10")
-    app.max_x.setText("10")
+def test_invalid_function(app, qtbot):
+    app.function_x.setText("5*y^2+2*x")
+    app.min_x.setText("-20")
+    app.max_x.setText("20")
     qtbot.mouseClick(app.plot_button, QtCore.Qt.LeftButton)
-    assert app.err == "error: enter a valid function"
+    assert app.error_messag == "error: enter a valid function"
 
-    app.f_x.setText("y")
+    app.function_x.setText("y")
     qtbot.mouseClick(app.plot_button, QtCore.Qt.LeftButton)
-    assert app.err == "error: Only numbers, operators (+, -, *, /) and the variable x are allowed"
+    assert app.error_messag == "error: you have to use only numbers , variable x and supported operators[+ - / * ^]"
 
 
-def invalid_limits(app, qtbot):
-    app.f_x.setText("5*X^2+2%x")
+def test_invalid_limits(app, qtbot):
+    app.function_x.setText("5*X^2+2%x")
 
     app.min_x.setText("test")
     app.max_x.setText("20")
     qtbot.mouseClick(app.plot_button, QtCore.Qt.LeftButton)
-    assert app.err == "error: enter valid limits"
+    assert app.error_messag == "error: enter valid lower limmit"
 
-    app.min_x.setText("--20")
-    app.max_x.setText("20test")
+    app.min_x.setText("-20")
+    app.max_x.setText("test")
     qtbot.mouseClick(app.plot_button, QtCore.Qt.LeftButton)
-    assert app.err == "error: enter valid limit numbers"
+    assert app.error_messag == "error:enter valid upper limmit"
 
-
-def valid_function_1(app, qtbot):
-    app.f_x.setText("5*X^2")
-    app.min_x.setText("-10")
-    app.max_x.setText("10")
+    
+    app.min_x.setText("20")
+    app.max_x.setText("-20")
     qtbot.mouseClick(app.plot_button, QtCore.Qt.LeftButton)
-    assert app.err == None
+    assert app.error_messag == "error: Lower limit must be less than the upper limit"
 
 
-def valid_function_2(app, qtbot):
-    app.f_x.setText("20^X+50*x+40")
-    app.min_x.setText("-10")
-    app.max_x.setText("10")
+
+def test_valid_function_1(app, qtbot):
+    app.function_x.setText("5*X^2")
+    app.min_x.setText("-20")
+    app.max_x.setText("20")
     qtbot.mouseClick(app.plot_button, QtCore.Qt.LeftButton)
-    assert app.err == None
+    assert app.error_messag == None
+
+
+def test_valid_function_2(app, qtbot):
+    app.function_x.setText("20^X+50*x+40")
+    app.min_x.setText("-20")
+    app.max_x.setText("20")
+    qtbot.mouseClick(app.plot_button, QtCore.Qt.LeftButton)
+    assert app.error_messag == None
 
